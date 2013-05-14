eccabbd57847461f3e995c9def1fd1543fcaaa93
Alex Smith <alex@alexsmith.org>
2013-05-14 23:50:24 +0100

    adding gzip

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.compress());
   app.use(express.static(__dirname + '/public'));
   app.use(app.router);
 });
